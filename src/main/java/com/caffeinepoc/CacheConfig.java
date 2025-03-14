6ed81d51c6c10116ce3c106cf378fe4ae2bafcfb
raulmartinez <raulmartinez@paradigmadigital.com>
2025-03-14 16:24:40 +0100

    POC

--- src/main/java/com/caffeinepoc/CacheConfig.java
+++ src/main/java/com/caffeinepoc/CacheConfig.java
@@ -1,9 +1,9 @@
 package com.caffeinepoc;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.concurrent.TimeUnit;
-
+import com.github.benmanes.caffeine.cache.Caffeine;
+import com.github.benmanes.caffeine.cache.RemovalCause;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.cache.CacheManager;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.cache.caffeine.CaffeineCache;
@@ -12,15 +12,19 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
-import com.github.benmanes.caffeine.cache.Caffeine;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.TimeUnit;
 
 @Configuration
 @EnableCaching
 @ComponentScan("com.caffeinepoc")
 public class CacheConfig {
 
+    private Logger logger = LoggerFactory.getLogger(CacheConfig.class);
+
+
     public static final String SOURCE_INFO_CACHE = "SOURCE_INFO";
-    public static final String PAYMENTMETHOD_INFO_CACHE = "PAYMENTMETHOD_INFO";
 
 
     @Bean
@@ -28,7 +32,6 @@ public class CacheConfig {
         List<CaffeineCache> caffeineCaches = new ArrayList<CaffeineCache>();
         //Expirará e 1 minuto con un size de 500 elementos
         caffeineCaches.add(buildCaffeineCache(SOURCE_INFO_CACHE,1L,TimeUnit.MINUTES,500L));
-        //caffeineCaches.add(buildCaffeineCache(PAYMENTMETHOD_INFO_CACHE,1L,TimeUnit.MINUTES,500L));
 
 
         SimpleCacheManager cacheManager = new SimpleCacheManager();
@@ -46,7 +49,14 @@ public class CacheConfig {
      * @param size
      * @return
      */
-    private static CaffeineCache buildCaffeineCache(String cacheName,long ttl, TimeUnit ttlUnit,long size) {
-        return new CaffeineCache(cacheName,Caffeine.newBuilder().expireAfterWrite(ttl, ttlUnit).maximumSize(size).build() );
+    private  CaffeineCache buildCaffeineCache(String cacheName,long ttl, TimeUnit ttlUnit,long size) {
+        return new CaffeineCache(cacheName, Caffeine.newBuilder()
+            .expireAfterWrite(ttl, ttlUnit)
+            .maximumSize(size)
+            .removalListener((Object key, Object value, RemovalCause cause) ->
+                logger.info(String.format("Key %s was removed (%s)%n", key, cause)))
+            .recordStats()
+            .build());
+
     }
 }
