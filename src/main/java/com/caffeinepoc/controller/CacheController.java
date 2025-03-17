3b5f85438d09e2e10ebefea3e6dc8df03260608e
raulmartinez <raulmartinez@paradigmadigital.com>
2025-03-17 18:10:19 +0100

    POC changes

---
+++ src/main/java/com/caffeinepoc/controller/CacheController.java
@@ -0,0 +1,43 @@
+package com.caffeinepoc.controller;
+
+import com.github.benmanes.caffeine.cache.Cache;
+import com.github.benmanes.caffeine.cache.stats.CacheStats;
+import org.springframework.cache.CacheManager;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.Set;
+
+@RestController
+@RequestMapping("admin/cache")
+public class CacheController {
+	private CacheManager cacheManager;
+
+	public CacheController(CacheManager cacheManager) {
+		this.cacheManager = cacheManager;
+	}
+
+	@GetMapping()
+	public List<cacheInfo> getCacheInfo() {
+		return cacheManager.getCacheNames()
+			.stream()
+			.map(this::getCacheInfo)
+			.toList();
+	}
+
+	private cacheInfo getCacheInfo(String cacheName) {
+		Cache<Object, Object> nativeCache =
+			(Cache)cacheManager.getCache(cacheName).getNativeCache();
+		Set<Object> keys = nativeCache.asMap().keySet();
+		CacheStats stats = nativeCache.stats();
+		return new cacheInfo(
+			cacheName, keys.size(), keys, stats.toString());
+	}
+
+	private record cacheInfo(
+		String name, int size, Set<Object> keys, String stats) {}
+
+}
--- src/main/java/com/caffeinepoc/repository/entity/Source.java
+++ src/main/java/com/caffeinepoc/repository/entity/Source.java
@@ -2,7 +2,6 @@ package com.caffeinepoc.repository.entity;
 
 import lombok.Builder;
 import lombok.Data;
-import lombok.NoArgsConstructor;
 
 @Data
 @Builder
